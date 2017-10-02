bcea289c15e81482eaecaa77d02fff7917a19130
Ben Albritton <benalbritton@outlook.com>
2017-10-02 08:53:18 -0500

    Moved to unittest folder

--- unittests/CheckWinLogic_Unittest.py
+++ unittests/CheckWinLogic_Unittest.py
@@ -1,12 +1,34 @@
-import unittest
+#This can run independently
+import unittest, sys, os
+
+#modifying sys path to import winLogic
+lib_path = os.path.abspath(os.path.join('..','Game'))
+sys.path.append(lib_path)
+
 from checkWinLogic import WinLogic
 
+#Each def test a specific patern. Hardcoded, yes, but the failure output list per function so 
 class CheckWinLogic(unittest.TestCase):
-	#Check Collumns
-	def test_temp_hasPlayerWon_CollumnOne(self): #no winner
+	def test_ClearMatrix_GetGrid_SetGrid(self):
+		win = WinLogic()
+		self.assertEqual(win.GetGrid(0,0),False)
+		win.SetGrid("TEST",0,0)
+		self.assertEqual(win.GetGrid(0,0),"TEST")
+		win.ClearMatrix()
+		self.assertEqual(win.GetGrid(0,0),False)
+
+	def test_SetPlayer_GetPlayer(self):
+		win = WinLogic()
+		self.assertEqual(win.GetPlayer(),"")
+		win.SetPlayer("PlayerOne")
+		self.assertEqual(win.GetPlayer(),"PlayerOne")
+		win.SetPlayer("PlayerTwo")
+		self.assertEqual(win.GetPlayer(),"PlayerTwo")
+		
+	#check win logic
+	def test_hasPlayerWon_CollumnOne(self): #no winner
 		#Make Class
 		win = WinLogic()
-		#Check Vertical
 		player = "Player1"
 		win.SetPlayer(player)
 		win.ClearMatrix()
@@ -14,10 +36,9 @@ class CheckWinLogic(unittest.TestCase):
 			win.SetGrid(player,0,i)
 		self.assertTrue(win.hasPlayerWon(player)) #win case 1
 
-	def test_temp_hasPlayerWon_CollumnTwo(self): #no winner
+	def test_hasPlayerWon_CollumnTwo(self): #no winner
 		#Make Class
 		win = WinLogic()
-		#Check Vertical
 		player = "Player1"
 		win.SetPlayer(player)
 		win.ClearMatrix()
@@ -25,22 +46,19 @@ class CheckWinLogic(unittest.TestCase):
 			win.SetGrid(player,1,i)
 		self.assertTrue(win.hasPlayerWon(player)) #win case 2
 		
-	def test_temp_hasPlayerWon_CollumnThree(self): #no winner
+	def test_hasPlayerWon_CollumnThree(self): #no winner
 		#Make Class
 		win = WinLogic()
-		#Check Vertical
 		player = "Player1"
 		win.SetPlayer(player)
 		win.ClearMatrix()
 		for i in range(3):
 			win.SetGrid(player,2,i)
-		self.assertTrue(win.hasPlayerWon(player)) #win case 3
-		
+		self.assertTrue(win.hasPlayerWon(player)) #win case 3	
 	#Check Rows
-	def test_temp_hasPlayerWon_RowOne(self): #no winner
+	def test_hasPlayerWon_RowOne(self): #no winner
 		#Make Class
 		win = WinLogic()
-		#Check Vertical
 		player = "Player1"
 		win.SetPlayer(player)
 		win.ClearMatrix()
@@ -48,10 +66,9 @@ class CheckWinLogic(unittest.TestCase):
 			win.SetGrid(player,i,0)
 		self.assertTrue(win.hasPlayerWon(player)) #win case 4
 
-	def test_temp_hasPlayerWon_RowTwo(self): #no winner
+	def test_hasPlayerWon_RowTwo(self): #no winner
 		#Make Class
 		win = WinLogic()
-		#Check Vertical
 		player = "Player1"
 		win.SetPlayer(player)
 		win.ClearMatrix()
@@ -59,20 +76,20 @@ class CheckWinLogic(unittest.TestCase):
 			win.SetGrid(player,i,1)
 		self.assertTrue(win.hasPlayerWon(player)) #win case 5
 		
-	def test_temp_hasPlayerWon_RowThree(self): #no winner
+	def test_hasPlayerWon_RowThree(self): #no winner
+		
 		#Make Class
 		win = WinLogic()
-		#Check Vertical
 		player = "Player1"
 		win.SetPlayer(player)
 		win.ClearMatrix()
 		for i in range(3):
 			win.SetGrid(player,i,2)
 		self.assertTrue(win.hasPlayerWon(player)) #win case 6
-	def test_temp_hasPlayerWon_DiagOne(self): #no winner
+		
+	def test_hasPlayerWon_DiagOne(self): #no winner
 		#Make Class
 		win = WinLogic()
-		#Check Vertical
 		player = "Player1"
 		win.SetPlayer(player)
 		win.ClearMatrix()
@@ -81,10 +98,9 @@ class CheckWinLogic(unittest.TestCase):
 		win.SetGrid(player,2,2)
 		self.assertTrue(win.hasPlayerWon(player)) #win case 7
 		
-	def test_temp_hasPlayerWon_DiagTwo(self): #no winner
+	def test_hasPlayerWon_DiagTwo(self): #no winner
 		#Make Class
 		win = WinLogic()
-		#Check Vertical
 		player = "Player1"
 		win.SetPlayer(player)
 		win.ClearMatrix()
@@ -92,8 +108,29 @@ class CheckWinLogic(unittest.TestCase):
 		win.SetGrid(player,1,1)
 		win.SetGrid(player,2,0)
 		self.assertTrue(win.hasPlayerWon(player)) #win case 8
+		
+	def test_hasPlayerWon_LoseCaseOne(self): #no winner
+		#Make Class
+		win = WinLogic()
+		player = "Player1"
+		win.SetPlayer(player)
+		win.ClearMatrix()
+		for i in range(2):
+			win.SetGrid(player,0,i)
+		self.assertFalse(win.hasPlayerWon(player)) #lose case 1
+	
+	def test_hasPlayerWon_LoseCaseTwo(self): #no winner
+		#Make Class
+		win = WinLogic()
+		player = "Player1"
+		win.SetPlayer(player)
+		win.ClearMatrix()
+		for i in range(2):
+			win.SetGrid(player,0,i)
+		win.SetGrid("Player2",0,2)
+		self.assertFalse(win.hasPlayerWon(player)) #lose case 1
 
-unittest.main()
-#if __name__ == '__main__':
+#unittest.main()
+if __name__ == '__main__':
 	#hasPayerWon()
-    #unittest.main()
+    unittest.main()
