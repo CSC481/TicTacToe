ee3de6321488e60d2e911312dc84617230e0471c
Ben Albritton <benalbritton@outlook.com>
2017-10-02 08:56:19 -0500

    Updated to try to work from the base directory
    Still Broken

--- UnitTestDriver.py
+++ UnitTestDriver.py
@@ -1,8 +1,14 @@
-import unittest
-#import the actual unit test
-import CheckWinLogic_Unittest
+import unittest, os, sys
+import os, sys
 
+lib_path = os.path.abspath(os.path.join('unittests'))
+sys.path.append(lib_path)
+
+#import the actual unit test
+#import CheckWinLogic_Unittest
+import CheckWinLogic_UT
+import helloWorld
 print "\n###################\nChecking CheckWinLogic...\n###################\n"
-suite = unittest.TestLoader().loadTestsFromModule(CheckWinLogic_Unittest)
-unittest.TextTestRunner(verbosity=2).run(suite)
+#suite = unittest.TestLoader().loadTestsFromModule(CheckWinLogic_Unittest)
+#unittest.TextTestRunner(verbosity=2).run(suite)
 
