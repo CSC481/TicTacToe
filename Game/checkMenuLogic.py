ffa5ca39c00da7450b0378545a55abfd82638a33
McKenzie Hafkemeyer <mckenzie.hafkemeyer@gmail.com>
2017-10-02 22:51:03 -0500

    Implement Menu Logic - setPlayerName()
    Wrote code to make unit test pass.

--- Game/checkMenuLogic.py
+++ Game/checkMenuLogic.py
@@ -7,7 +7,16 @@ class MenuLogic:
         self.player2 = ""
 
     def setPlayerName(self, playerNumber, playerName):
-        return
+        if playerNumber == 1:
+            if playerName.strip() == "":
+                self.player1 = "Player 1"
+            else:
+                self.player1 = playerName
+        if playerNumber == 2:
+            if playerName.strip() == "":
+                self.player2 = "Player 2"
+            else:
+                self.player2 = playerName
 
     def launchGame(self, playerName1, playerName2):
         return
