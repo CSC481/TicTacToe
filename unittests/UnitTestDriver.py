26407bb84133bf2f7d1aefef38bfecb4d2d42e76
Ben Albritton <benalbritton@outlook.com>
2017-10-04 11:42:12 -0500

    Wrote unit test and updated testdriver for turnmanagment

--- unittests/UnitTestDriver.py
+++ unittests/UnitTestDriver.py
@@ -2,8 +2,13 @@ import unittest, os, sys
 
 #import the actual unit test
 import CheckWinLogic_Unittest
+import TurnManagement_Unittest
 
 print ("\n###################\nChecking CheckWinLogic...\n###################\n")
-suite = unittest.TestLoader().loadTestsFromModule(CheckWinLogic_Unittest)
-unittest.TextTestRunner(verbosity=2).run(suite)
+Test_CheckWinLogic = unittest.TestLoader().loadTestsFromModule(CheckWinLogic_Unittest)
+unittest.TextTestRunner(verbosity=2).run(Test_CheckWinLogic)
+
+print ("\n###################\nChecking TurnManagement...\n###################\n")
+Test_TurnManagement = unittest.TestLoader().loadTestsFromModule(TurnManagement_Unittest)
+unittest.TextTestRunner(verbosity=2).run(Test_TurnManagement)
 
